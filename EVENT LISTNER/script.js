c7dbff4f5ab8d35ce429651cedc186bc0f9fb3ae
task-masterr01 <hemant.here96@gmail.com>
2026-02-25 23:49:47 +0530

    add data

---
+++ EVENT LISTNER/script.js
@@ -0,0 +1,21 @@
+// function handleclick(){
+//     const element = document.getElementById("first");
+//     element.textContent = "text have been changed";
+// }
+
+// this is also not an efficient way to using event listner
+// const element = document.getElementById("first");
+// element.onclick = function handleClick(){
+//     element.textContent = "text data have been changed";
+// }
+
+// instead we can use addEventListner()
+ const element = document.getElementById("first");
+ element.addEventListener("click" , ()=>{
+    element.textContent = "text data has been changed";
+ })
+ element.addEventListener("click" , ()=>{
+    element.style.backgroundColor = "brown";
+ })
+ // we can add multiple event listner's using addEventListner
+ 
