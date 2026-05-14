87d4c519d7ad400b61495cb4482e187ca098afe1
task-masterr01 <hemant.here96@gmail.com>
2026-05-14 19:28:20 +0530

    add data to js file

--- JS PROJECTS/P5-TODO LIST/script.js
+++ JS PROJECTS/P5-TODO LIST/script.js
@@ -1,9 +1,54 @@
 let tasks = [];
 let editingId = null;
 function loadData() {
-    const saved = localStorage.getItem() ;
+    tasks = [
+        {
+            id : 1 ,
+            title : "eveluate the adddition and deletion of task",
+            status : "pending" ,
+            priority : "critical" ,
+            completed : false,
+
+        },
+        {
+            id : 2,
+            title : "identify the implementation team",
+            status : "progress" ,
+            priority : "minor" ,
+            completed : false,
+        },
+        {
+            id : 3,
+            title : "batch schedule download/progress",
+            status : "pending" ,
+            priority : "critical" ,
+            completed : false,
+        },
+        {
+            id : 4,
+            title : "monitor system performance and adjust hardware",
+            status : "pending" ,
+            priority : "minor" ,
+            completed : false,
+        }
+    ];
+    updateGreeting();
+    renderTasks();   
+}
+function updateGreeting() {
+    const hours = new Date().getHours() ;
+    let greet = "Good Morning" ;
+
+    if ( hours >= 12 && hours < 18  ) greet = "Good Afternoon " 
+    else if ( hours >= 18 &&  hours < 20 ) greet = "Good Evening  " 
+    else greet = "Good Night" 
+    document.getElementById("greeting").textContent = `${greet} , Sweet Heart !` ;
+}
+
+function saveData () {
     
 }
+
 
 
 
